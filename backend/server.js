b2e78358e7bcc687882ee1c469c191cd5532c343
Ivan Augusto Hoinacki <ivanhweb@gmail.com>
2016-09-09 22:25:51 -0300

    update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
-
+// npm install --save body-parser
 const PORT = 8081;
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -48,42 +54,45 @@ var listaContatos = [{
     }
 }];
 
-var listaOperadoras = [
-    {
-        nome: 'Vivo',
-        id: 1,
-        categoria: 'Movel',
-        valor: 96
-    }, {
-        nome: 'Nextel',
-        id: 6,
-        categoria: 'Fixo',
-        valor: 85
-    }, {
-        nome: 'Claro',
-        id: 2,
-        categoria: 'Movel',
-        valor: 85
-    }, {
-        nome: 'Tim',
-        id: 3,
-        categoria: 'Movel',
-        valor: 65
-    }, {
-        nome: 'Oi',
-        id: 4,
-        categoria: 'Movel',
-        valor: 50
-    }, {
-        nome: 'GVT',
-        id: 5,
-        categoria: 'Fixo',
-        valor: 2
-    }
-];
+var listaOperadoras = [{
+    nome: 'Vivo',
+    id: 1,
+    categoria: 'Movel',
+    valor: 96
+}, {
+    nome: 'Nextel',
+    id: 6,
+    categoria: 'Fixo',
+    valor: 85
+}, {
+    nome: 'Claro',
+    id: 2,
+    categoria: 'Movel',
+    valor: 85
+}, {
+    nome: 'Tim',
+    id: 3,
+    categoria: 'Movel',
+    valor: 65
+}, {
+    nome: 'Oi',
+    id: 4,
+    categoria: 'Movel',
+    valor: 50
+}, {
+    nome: 'GVT',
+    id: 5,
+    categoria: 'Fixo',
+    valor: 2
+}];
 
 app.get('/contatos', function(req, res) {
     res.json(listaContatos);
+});
+
+app.post('/contatos', function(req, res) {
+    listaContatos.push(req.body);
+    res.json(true);
 });
 
 app.get('/operadoras', function(req, res) {
